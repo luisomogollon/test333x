a881e8499a223c644f5e0e6ee72afce05b5372fe
luisomogollon <madwinrar@gmail.com>
2025-02-20 19:47:27 -0400

    ++

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -371,7 +371,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingHorizontal: 16,
-    paddingTop: 60,
+    paddingTop: 30,
     paddingBottom: 16,
   },
   headerRight: {
